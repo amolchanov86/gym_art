c5be4b83d2d27a8e31e1c02f1f581693035f8c99
Artem Molchanov <a.molchanov86@gmail.com>
2019-01-23 22:14:15 -0800

    added plotting randomization stats

---
+++ gym_art/quadrotor/plot_randomization_stats.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./quadrotor.py -q crazyflie -dre 1 -drr 0.01 -pltdyn -r -trj 100
--- gym_art/quadrotor/quadrotor.py
+++ gym_art/quadrotor/quadrotor.py
@@ -1148,13 +1148,14 @@ class QuadrotorEnv(gym.Env, Serializable):
         return self._step(action)
 
 
-def test_rollout(quad, dyn_randomize_every=None, dyn_randomization_ratio=None, render=True, traj_num=10, plot_step=None):
+def test_rollout(quad, dyn_randomize_every=None, dyn_randomization_ratio=None, render=True, traj_num=10, plot_step=None, plot_dyn_change=True):
     import tqdm
     #############################
     # Init plottting
-    fig = plt.figure(1)
-    # ax = plt.subplot(111)
-    plt.show(block=False)
+    if plot_step is not None:
+        fig = plt.figure(1)
+        # ax = plt.subplot(111)
+        plt.show(block=False)
 
     # render = True
     # plot_step = 50
@@ -1177,6 +1178,19 @@ def test_rollout(quad, dyn_randomize_every=None, dyn_randomization_ratio=None, r
         print('Action space:', env.action_space[0].spaces[0].low, env.action_space[0].spaces[0].high)
     # input('Press any key to continue ...')
 
+    ## Collected statistics for dynamics
+    dyn_param_names = [
+        "mass",
+        "inertia",
+        "thrust_to_weight",
+        "torque_to_thrust",
+        "thrust_noise_ratio",
+        "vel_damp",
+        "damp_omega_quadratic"
+    ]
+
+    dyn_param_stats = [[] for i in dyn_param_names]
+
     action = [0.5, 0.5, 0.5, 0.5]
     rollouts_id = 0
 
@@ -1188,6 +1202,13 @@ def test_rollout(quad, dyn_randomize_every=None, dyn_randomization_ratio=None, r
         ## Diagnostics
         observations = []
         velocities = []
+
+        ## Collecting dynamics params
+        if plot_dyn_change:
+            # import pdb; pdb.set_trace()
+            for par_i, par in enumerate(dyn_param_names):
+                dyn_param_stats[par_i].append(np.array(getattr(env.dynamics, par)).flatten())
+                # print(par, dyn_param_stats[par_i][-1])
 
         t = 0
         while True:
@@ -1212,12 +1233,44 @@ def test_rollout(quad, dyn_randomize_every=None, dyn_randomization_ratio=None, r
 
             if done: break
             t += 1
+
+    if plot_dyn_change:
+        dyn_par_normvar = []
+        dyn_par_means = []
+        dyn_par_var = []
+        plt.figure(2, figsize=(10, 10))
+        for par_i, par in enumerate(dyn_param_stats):
+            plt.subplot(3, 3, par_i+1)
+            par = np.array(par)
+
+            ## Compute stats
+            # print(dyn_param_names[par_i], par)
+            dyn_par_means.append(np.mean(par, axis=0))
+            dyn_par_var.append(np.std(par, axis=0))
+            dyn_par_normvar.append(dyn_par_var[-1] / dyn_par_means[-1])
+
+            # import pdb; pdb.set_trace()
+            if par.shape[1] > 1:
+                for vi in range(par.shape[1]):
+                    plt.plot(par[:, vi])
+            else:
+                plt.plot(par)
+            # plt.title(dyn_param_names[par_i] + "\n Normvar: %s" % str(dyn_par_normvar[-1]))
+            plt.title(dyn_param_names[par_i])
+            print(dyn_param_names[par_i], "NormVar: ", dyn_par_normvar[-1])
+    
+
+
+
+
     print("##############################################################")
     print("Total time: ", time.time() - start_time )
+
     # print('Rollouts are done!')
     # plt.pause(2.0)
     # plt.waitforbuttonpress()
-    if plot_step is not None:
+    if plot_step is not None or plot_dyn_change:
+        plt.show(block=False)
         input("Press Enter to continue...")
 
 def main(argv):
@@ -1265,6 +1318,11 @@ def main(argv):
         type=int,
         help="Plot step"
     )
+    parser.add_argument(
+        '-pltdyn',"--plot_dyn_change",
+        action="store_true",
+        help="Plot the dynamics change from trajectory to trajectory?"
+    )
     args = parser.parse_args()
 
     if args.mode == 0:
@@ -1275,7 +1333,8 @@ def main(argv):
             dyn_randomization_ratio=args.dyn_randomization_ratio,
             render=args.render,
             traj_num=args.traj_num,
-            plot_step=args.plot_step
+            plot_step=args.plot_step,
+            plot_dyn_change=args.plot_dyn_change
         )
 
 if __name__ == '__main__':
