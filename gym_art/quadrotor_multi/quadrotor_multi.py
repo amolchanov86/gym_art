9122e4503abe820fe39aee2ce17179c88fa715d2
Sumeet <sscifi@gmail.com>
2020-12-14 08:11:28 -0700

    Bezier goals always sample within the boundaries

--- gym_art/quadrotor_multi/quadrotor_multi.py
+++ gym_art/quadrotor_multi/quadrotor_multi.py
@@ -346,17 +346,23 @@ class QuadrotorEnvMulti(gym.Env):
             max_dist = min(30, self.room_dims[0])
             min_dist = max_dist / 2
             if tick % control_steps == 0 or tick == 1:
-                low, high = -np.array(self.room_dims), np.array(self.room_dims)
-                new_pos = np.random.uniform(low=low, high=high,
-                                            size=(2, 3)).reshape(3, 2)
-                new_pos = new_pos * np.random.randint(min_dist, max_dist+1) / np.linalg.norm(new_pos) # add some velocity randomization
-                new_pos = self.goal[0].reshape(3, 1) + new_pos
+                # sample a new goal pos that's within the room boundaries and satisfies the distance constraint
+                new_goal_found = False
+                while not new_goal_found:
+                    low, high = np.array([-self.room_dims[0], -self.room_dims[1], 0]), np.array(self.room_dims)
+                    new_pos = np.random.uniform(low=low, high=high,
+                                                size=(2, 3)).reshape(3, 2)  # need an intermediate point for a deg=2 curve
+                    new_pos = new_pos * np.random.randint(min_dist, max_dist+1) / np.linalg.norm(new_pos, axis=0) # add some velocity randomization
+                    new_pos = self.goal[0].reshape(3, 1) + new_pos
+                    lower_bound = np.expand_dims(low, axis=1)
+                    upper_bound = np.expand_dims(high, axis=1)
+                    new_goal_found = (new_pos > lower_bound).all() and (new_pos < upper_bound).all()
                 nodes = np.concatenate((self.goal[0].reshape(3, 1), new_pos), axis=1)
                 nodes = np.asfortranarray(nodes)
                 pts = np.linspace(0, 1, control_steps)
                 curve = bezier.Curve(nodes, degree=2)
                 self.interp = curve.evaluate_multi(pts)
-                self.interp = np.clip(self.interp, a_min=np.array([0,0,0.2]).reshape(3,1), a_max=high.reshape(3,1)) # want goal clipping to be slightly above the floor
+                # self.interp = np.clip(self.interp, a_min=np.array([0,0,0.2]).reshape(3,1), a_max=high.reshape(3,1)) # want goal clipping to be slightly above the floor
             if tick % control_steps != 0 and tick > 1:
                 self.goal = [self.interp[:, t] for _ in range(self.num_agents)]
                 self.goal = np.array(self.goal)
