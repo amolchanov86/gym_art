4e228ef4ff3c815a5d27712671a9f66d2cff7fbc
Aleksei Petrenko <petrenko@usc.edu>
2021-01-09 15:06:41 -0800

    Scenarios refactoring

--- gym_art/quadrotor_multi/quad_scenarios.py
+++ gym_art/quadrotor_multi/quad_scenarios.py
@@ -2,13 +2,21 @@ import numpy as np
 import random
 import bezier
 
+from gym_art.quadrotor_multi.quad_utils import generate_points
 
-class QuadrotorScenario():
-    def __init__(self, envs, pos, goal, settle_count, num_agents, room_dims,
-                 rews_settle_raw, rews_settle, rew_coeff):
+
+def create_scenario(quads_mode, envs, pos, settle_count, num_agents, room_dims,
+                    rews_settle_raw, rews_settle, rew_coeff, quads_formation, quads_formation_size):
+    cls = eval('Scenario_' + quads_mode)
+    scenario = cls(envs, pos, settle_count, num_agents, room_dims, rews_settle_raw, rews_settle, rew_coeff, quads_formation, quads_formation_size)
+    return scenario
+
+
+class QuadrotorScenario:
+    def __init__(self, envs, pos, settle_count, num_agents, room_dims,
+                 rews_settle_raw, rews_settle, rew_coeff, quads_formation, quads_formation_size):
         self.envs = envs
         self.pos = pos
-        self.goal = goal
         self.settle_count = settle_count
         self.num_agents = num_agents
         self.room_dims = room_dims
@@ -18,10 +26,66 @@ class QuadrotorScenario():
 
         self.interp = None
 
-    def circular_config(self, infos, rewards):
+        self.cfg_quads_formation = quads_formation
+        self.cfg_quads_formation_size = quads_formation_size
+
+        self.formation = self.formation_size = None
+
+        self.goals = None
+
+    def get_formation(self):
+        return "circle"
+
+    def get_formation_size(self):
+        return 0.0
+
+    def setup_formation(self):
+        if self.cfg_quads_formation == "default":
+            self.formation = self.get_formation()
+        else:
+            self.formation = self.cfg_quads_formation
+
+        if self.cfg_quads_formation_size < 0:
+            self.formation_size = self.get_formation_size()
+        else:
+            self.formation_size = self.cfg_quads_formation_size
+
+    def init_goals(self):
+        self.setup_formation()
+
+        pi = np.pi
+
+        goals = []
+
+        if self.formation == "circle":
+            for i in range(self.num_agents):
+                degree = 2 * pi * i / self.num_agents
+                goal_x = self.formation_size * np.cos(degree)
+                goal_y = self.formation_size * np.sin(degree)
+                goal = [goal_x, goal_y, 2.0]
+                goals.append(goal)
+
+            goals = np.array(goals)
+        elif self.formation == "sphere":
+            goals = self.formation_size * np.array(generate_points(self.num_agents))
+            goals[:, 2] += 2.0
+        else:
+            raise NotImplementedError("Unknown formation")
+
+        self.goals = goals
+
+    def step(self, infos, rewards):
+        raise NotImplementedError("Implemented in a specific scenario")
+
+
+class Scenario_circular_config(QuadrotorScenario):
+    def get_formation_size(self):
+        return 0.5
+
+    def step(self, infos, rewards):
         for i, e in enumerate(self.envs):
             dist = np.linalg.norm(self.pos[i] - e.goal)
-            if abs(dist) < 0.02:
+            if abs(dist) < 0.05:
                 self.settle_count[i] += 1
             else:
                 self.settle_count = np.zeros(self.num_agents)
@@ -30,9 +94,9 @@ class QuadrotorScenario():
         control_step_for_one_sec = int(self.envs[0].control_freq)
         tmp_count = self.settle_count >= control_step_for_one_sec
         if all(tmp_count):
-            np.random.shuffle(self.goal)
+            np.random.shuffle(self.goals)
             for i, env in enumerate(self.envs):
-                env.goal = self.goal[i]
+                env.goal = self.goals[i]
                 # Add settle rewards
                 self.rews_settle_raw[i] = control_step_for_one_sec
                 self.rews_settle[i] = self.rew_coeff["quadsettle"] * self.rews_settle_raw[i]
@@ -44,7 +108,26 @@ class QuadrotorScenario():
             self.rews_settle_raw = np.zeros(self.num_agents)
             self.settle_count = np.zeros(self.num_agents)
 
-    def dynamic_goal(self):
+
+class Scenario_static_goal(QuadrotorScenario):
+    def get_formation(self):
+        return 'circle'
+
+    def get_formation_size(self):
+        return 0.0
+
+    def step(self, infos, rewards):
+        pass
+
+
+class Scenario_dynamic_goal(QuadrotorScenario):
+    def get_formation(self):
+        return 'circle'
+
+    def get_formation_size(self):
+        return 0.0
+
+    def step(self, infos, rewards):
         tick = self.envs[0].tick
         # teleport every 5 secs
         control_step_for_five_sec = int(5.0 * self.envs[0].control_freq)
@@ -56,28 +139,49 @@ class QuadrotorScenario():
             if z < 0.25:
                 z = 0.25
 
-            self.goal = [[x, y, z] for i in range(self.num_agents)]
-            self.goal = np.array(self.goal)
+            goal = [[x, y, z] for i in range(self.num_agents)]
+            self.goals = np.array(goal)
 
             for i, env in enumerate(self.envs):
-                env.goal = self.goal[i]
+                env.goal = self.goals[i]
 
-    def static_goal(self):
-        pass
 
-    def ep_lissajous3D(self):
+class Scenario_ep_lissajous3D(QuadrotorScenario):
+    def get_formation(self):
+        return 'circle'
+
+    def get_formation_size(self):
+        return 0.0
+
+    # Based on https://mathcurve.com/courbes3d.gb/lissajous3d/lissajous3d.shtml
+    @staticmethod
+    def lissajous3D(tick, a=0.03, b=0.01, c=0.01, n=2, m=2, phi=90, psi=90):
+        x = a * np.sin(tick)
+        y = b * np.sin(n * tick + phi)
+        z = c * np.cos(m * tick + psi)
+        return x, y, z
+
+    def step(self, infos, rewards):
         control_freq = self.envs[0].control_freq
         tick = self.envs[0].tick / control_freq
         x, y, z = self.lissajous3D(tick)
-        goal_x, goal_y, goal_z = self.goal[0][0], self.goal[0][1], self.goal[0][2]
+        goal_x, goal_y, goal_z = self.goals[0][0], self.goals[0][1], self.goals[0][2]
         x_new, y_new, z_new = x + goal_x, y + goal_y, z + goal_z
-        self.goal = [[x_new, y_new, z_new] for i in range(self.num_agents)]
-        self.goal = np.array(self.goal)
+        goal = [[x_new, y_new, z_new] for i in range(self.num_agents)]
+        goal = np.array(goal)
 
         for i, env in enumerate(self.envs):
-            env.goal = self.goal[i]
+            env.goal = goal[i]
 
-    def ep_rand_bezier(self):
+
+class Scenario_ep_rand_bezier(QuadrotorScenario):
+    def get_formation(self):
+        return 'circle'
+
+    def get_formation_size(self):
+        return 0.0
+
+    def step(self, infos, rewards):
         # randomly sample new goal pos in free space and have the goal move there following a bezier curve
         tick = self.envs[0].tick
         control_freq = self.envs[0].control_freq
@@ -96,27 +200,34 @@ class QuadrotorScenario():
                     [self.room_dims[0] / 2, self.room_dims[1] / 2, self.room_dims[2]])
                 new_pos = np.random.uniform(low=-high, high=high, size=(2, 3)).reshape(3,
                                                                                        2)  # need an intermediate point for  a deg=2 curve
-                new_pos = new_pos * np.random.randint(min_dist, max_dist + 1) / np.linalg.norm(new_pos,
-                                                                                               axis=0)  # add some velocity randomization = random magnitude * unit direction
-                new_pos = self.goal[0].reshape(3, 1) + new_pos
+                new_pos = new_pos * np.random.randint(min_dist, max_dist + 1) / np.linalg.norm(new_pos, axis=0)  # add some velocity randomization = random magnitude * unit direction
+                new_pos = self.goals[0].reshape(3, 1) + new_pos
                 lower_bound = np.expand_dims(low, axis=1)
                 upper_bound = np.expand_dims(high, axis=1)
                 new_goal_found = (new_pos > lower_bound + 0.5).all() and (
                         new_pos < upper_bound - 0.5).all()  # check bounds that are slightly smaller than the room dims
-            nodes = np.concatenate((self.goal[0].reshape(3, 1), new_pos), axis=1)
+            nodes = np.concatenate((self.goals[0].reshape(3, 1), new_pos), axis=1)
             nodes = np.asfortranarray(nodes)
             pts = np.linspace(0, 1, control_steps)
             curve = bezier.Curve(nodes, degree=2)
             self.interp = curve.evaluate_multi(pts)
             # self.interp = np.clip(self.interp, a_min=np.array([0,0,0.2]).reshape(3,1), a_max=high.reshape(3,1)) # want goal clipping to be slightly above the floor
         if tick % control_steps != 0 and tick > 1:
-            self.goal = [self.interp[:, t] for _ in range(self.num_agents)]
-            self.goal = np.array(self.goal)
+            self.goals = [self.interp[:, t] for _ in range(self.num_agents)]
+            self.goals = np.array(self.goals)
 
             for i, env in enumerate(self.envs):
-                env.goal = self.goal[i]
+                env.goal = self.goals[i]
 
-    def swarm_vs_swarm(self):
+
+class Scenario_swarm_vs_swarm(QuadrotorScenario):
+    def get_formation(self):
+        return 'grid_vertical'
+
+    def get_formation_size(self):
+        return 1.0
+
+    def step(self, infos, rewards):
         tick = self.envs[0].tick
         control_step_for_five_sec = int(5.0 * self.envs[0].control_freq)
         # Switch every 5th second
